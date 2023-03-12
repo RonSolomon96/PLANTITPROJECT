3e01eb1e31e113f62a28f62d8dd51a64ae06ad27
Ron Solomon <rsalomon230@gmail.com>
2023-03-12 21:42:43 +0200

    home screen navigation bar

--- plantit/lib/screens/homeScreen.dart
+++ plantit/lib/screens/homeScreen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
         appBar: AppBar(
           title: const Text('Plant App'),
         ),
